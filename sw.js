87d957525c070c0cf23695f8991a715c8f5f5164
ODILeedsBot <stuart.lowe@odileeds.org>
2019-08-09 13:21:43 +0100

    Update Service Worker URLs

--- sw.js
+++ sw.js
@@ -2,13 +2,13 @@ self.addEventListener('install', function(e) {
 	e.waitUntil(
 		caches.open('video-store').then(function(cache) {
 			return cache.addAll([
-				'/projects/bins/',
-				'/projects/bins/index.html',
-				'/projects/bins/stuquery.js',
-				'/projects/bins/bins.css',
-				'/projects/bins/bins.js',
-				'/projects/bins/data/list.json',
-				'/projects/bins/data/leeds/index.csv'
+				'/bins/',
+				'/bins/index.html',
+				'/bins/stuquery.js',
+				'/bins/bins.css',
+				'/bins/bins.js',
+				'/bins/data/list.json',
+				'/bins/data/leeds/index.csv'
 			]);
 		})
 	);
