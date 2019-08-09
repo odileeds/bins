15bb5c20d67dbe16a85545f781597d24a94206d0
ODILeedsBot <stuart.lowe@odileeds.org>
2019-08-09 13:12:02 +0100

    Update jobs

--- update.sh
+++ update.sh
@@ -1,2 +1,7 @@
-wget -o data/dm_premises.csv http://opendata.leeds.gov.uk/downloads/bins/dm_premises.csv
-wget -o data/leeds/dm_jobs.csv http://opendata.leeds.gov.uk/downloads/bins/dm_jobs.csv
+wget -O data/leeds/dm_premises.csv http://opendata.leeds.gov.uk/downloads/bins/dm_premises.csv
+wget -O data/leeds/dm_jobs.csv http://opendata.leeds.gov.uk/downloads/bins/dm_jobs.csv
+
+perl processBinData.pl
+git add data/leeds/*.*
+git commit -m "Update collection dates"
+git push
