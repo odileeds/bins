a91bdb8f67d62e503c6858842d34daf301938883
Stuart Lowe <gravatar@strudel.org.uk>
2019-08-14 16:14:15 +0100

    Update the cache

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', function(e) {
 	console.log('install ServiceWorker');
 	e.waitUntil(
-		caches.open('video-store').then(function(cache) {
+		caches.open('v1').then(function(cache) {
 			return cache.addAll([
 				'/bins/',
 				'/bins/index.html',
@@ -22,7 +22,15 @@ self.addEventListener('fetch', function(e) {
 	console.log('fetch '+e.request.url);
 	e.respondWith(
 		caches.match(e.request).then(function(response) {
-			return response || fetch(e.request);
+			return response || fetch(e.request).then((response) => {
+				// Store result in the cache https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
+				return caches.open('v1').then((cache) => {
+					cache.put(event.request, response.clone());
+					return response;
+				});  
+			});
+		}).catch(function(error){
+			return new Response('');
 		})
 	);
 });
